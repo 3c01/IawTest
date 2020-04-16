fc3294e79b082faf1c6e9c87f5f93845629ca4d8
3col <59328071+3c0l@users.noreply.github.com>
2020-04-16 19:08:48 +0200

    Test

---
+++ Data/Scripts/AI/BuildSpaceDefensiveStructurePlan.lua
@@ -0,0 +1,76 @@
+-- $Id: //depot/Projects/StarWars/Run/Data/Scripts/AI/BuildSpaceDefensiveStructurePlan.lua#10 $
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+-- (C) Petroglyph Games, Inc.
+--
+--
+--  *****           **                          *                   *
+--  *   **          *                           *                   *
+--  *    *          *                           *                   *
+--  *    *          *     *                 *   *          *        *
+--  *   *     *** ******  * **  ****      ***   * *      * *****    * ***
+--  *  **    *  *   *     **   *   **   **  *   *  *    * **   **   **   *
+--  ***     *****   *     *   *     *  *    *   *  *   **  *    *   *    *
+--  *       *       *     *   *     *  *    *   *   *  *   *    *   *    *
+--  *       *       *     *   *     *  *    *   *   * **   *   *    *    *
+--  *       **       *    *   **   *   **   *   *    **    *  *     *   *
+-- **        ****     **  *    ****     *****   *    **    ***      *   *
+--                                          *        *     *
+--                                          *        *     *
+--                                          *       *      *
+--                                      *  *        *      *
+--                                      ****       *       *
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+-- C O N F I D E N T I A L   S O U R C E   C O D E -- D O   N O T   D I S T R I B U T E
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+--
+--              $File: //depot/Projects/StarWars/Run/Data/Scripts/AI/BuildSpaceDefensiveStructurePlan.lua $
+--
+--    Original Author: James Yarrow
+--
+--            $Author: James_Yarrow $
+--
+--            $Change: 24353 $
+--
+--          $DateTime: 2005/08/19 17:54:05 $
+--
+--          $Revision: #10 $
+--
+--/////////////////////////////////////////////////////////////////////////////////////////////////
+
+require("pgevents")
+
+
+function Definitions()
+	DebugMessage("%s -- In Definitions", tostring(Script))
+	
+    Category = "Build_Space_Defensive_Structure"
+	IgnoreTarget = true
+	TaskForce = {
+	{
+		"StructureForce",
+		"Ground_Ion_Cannon | Ground_Empire_Hypervelocity_Gun | Ground_Rebel_Hypervelocity_Gun | E_Ground_Ion_Cannon | NewRep_XQ | Space_Structure = 1"
+	}
+	}
+
+	DebugMessage("%s -- Done Definitions", tostring(Script))
+end
+
+function StructureForce_Thread()
+	DebugMessage("%s -- In StructureForce_Thread.", tostring(Script))
+	
+	Sleep(1)
+	
+	StructureForce.Set_As_Goal_System_Removable(false)
+	AssembleForce(StructureForce)
+	StructureForce.Set_Plan_Result(true)
+	
+	DebugMessage("%s -- StructureForce done!", tostring(Script));
+	ScriptExit()
+end
+
+function StructureForce_Production_Failed(tf, failed_object_type)
+	DebugMessage("%s -- Abandonning plan owing to production failure.", tostring(Script))
+	ScriptExit()
+end
